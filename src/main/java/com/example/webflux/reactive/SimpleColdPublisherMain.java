4824b00592bb0633e24596df700d495eeadf490d
minutaurus <made_power@naver.com>
2024-03-27 19:43:42 +0900

    SimpleColdPublisher

---
+++ src/main/java/com/example/webflux/reactive/SimpleColdPublisherMain.java
@@ -0,0 +1,17 @@
+package com.example.webflux.reactive;
+
+import lombok.SneakyThrows;
+
+public class SimpleColdPublisherMain {
+    @SneakyThrows
+    public static void main(String[] args){
+        var publisher = new SimpleColdPublisher();
+        var subscriber = new SimpleNamedSubscriber<Integer>("subsriber1");
+        publisher.subscribe(subscriber);
+
+        Thread.sleep(5000);
+
+        var subscriber2 = new SimpleNamedSubscriber<Integer>("subscriber2");
+        publisher.subscribe(subscriber2);
+    }
+}
